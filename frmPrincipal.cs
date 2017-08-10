496ec94d8278ec2a4eeebaec9cfdfb080e9d1377
LgiMatBrd <contato@skorp.com.br>
2017-08-10 18:14:52 -0300

    correção botão iniciar

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -285,7 +285,7 @@ namespace VNJIngressos
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(230, 50);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Iniciar Venda de Ingresso - Guiche 7";
+            this.button2.Text = "Iniciar Venda de Ingresso";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
