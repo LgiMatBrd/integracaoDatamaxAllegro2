c0d18d475707626d24401faa3d221edcaceaedfe
LgiMatBrd <contato@skorp.com.br>
2017-09-27 15:40:19 -0300

    Removendo combobox Arrumando leitura de acessos do sistema

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -18,33 +18,13 @@ namespace VNJIngressos
     private Button button1;
     private Timer timer1;
     private Button button2;
-    private ComboBox comboBox1;
-    private Label label1;
     private Button button3;
 
-    string stringConexao = "SERVER=localhost;" + "DATABASE=vnj_local_novo;" + "UID=root;" + "PASSWORD=afornalli;";
+    string stringConexao = "SERVER=191.232.241.36;" + "DATABASE=app_hml;" + "UID=root;" + "PASSWORD=vnj!db2017!;";
 
     public frmPrincipal()
         {
             this.InitializeComponent();
-            // Popula o ComboBox
-            this.comboBox1.Items.Insert(0, "Selecione uma opção");
-            this.comboBox1.SelectedIndex = 0;
-            MySqlConnection connection = new MySqlConnection(stringConexao);
-            string command = "SELECT caixa_id,nome FROM caixa;";
-            MySqlDataAdapter da = new MySqlDataAdapter(command, connection);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            foreach (DataRow row in dt.Rows)
-            {
-                // SÓ FUNCIONA ESSA LINHA SE A TABELA ESTIVER NA ODERM!
-                //comboBox1.Items.Insert(Convert.ToInt16(row.ItemArray[0]), row.ItemArray[0].ToString());
-
-                // CASO NÃO ESTEJA NA ORDEM UTILIZAR ESSE CÓDIGO!
-                string rowz = string.Format("{0} - {1}", row.ItemArray[0], row.ItemArray[1]);
-                this.comboBox1.Items.Add(rowz);
-            }
-            connection.Close();
     }
 
     private void updateAcesso(frmPrincipal.ImpIngresso ing, MySqlConnection conn)
@@ -67,11 +47,36 @@ namespace VNJIngressos
       }
     }
 
-    private List<frmPrincipal.ImpIngresso> BuscarImpressos(string CAIXA)
+    private List<frmPrincipal.ImpIngresso> BuscarImpressos()
     {
         // BUSCA INGRESSOS
-        string str = "select evento.titulo as Titulo, " + "           ingresso.titulo as Ingresso, " + "           coalesce(ingressocategoria.preco,0) as Valor, " + "           acesso.acesso_id as Numero, " + "           campeonato.nrApoliceSeg as Apolice," + "           campeonato.nomeSeguradora as Seguradora, " + "           acesso.chaveAcesso as Barcode, " + "           arena_tipoingresso.titulo as legenda,   " + "           case when coalesce(ingressocategoria.preco, 0) = 0 then 'VENDA PROIBIDA' ELSE ' ' END AS Observacao, " + "           coalesce(arena_setor.nome,'') as Setor, " + "           coalesce(arena_fila.identificador,'') as Fila, " + "           coalesce(arena_cadeira.identificador,'') Cadeira, " + "           concat(evento.data, ' ', evento.horario) as DataHora, " + "           categoria.titulo, " + "           acesso.acesso_id " + "from acesso " + "left join evento                on evento.evento_id = acesso.evento_id " + "inner join arena_tipoingresso on arena_tipoingresso.tpingresso_id = acesso.tpingresso_id " + "inner join ingressocategoria on ingressocategoria.ingressocategoria_id = acesso.ingressocategoria_id" + " inner join ingresso on ingresso.ingresso_id = ingressocategoria.ingresso_id " + "inner join campeonato on campeonato.campeonato_id = evento.campeonato_id " + "inner join categoria on categoria.categoria_id = ingressocategoria.categoria_id " + "left join arena_cadeira on arena_cadeira.cadeira_id = acesso.cadeira_id " + "left join arena_fila on arena_fila.fila_id = arena_cadeira.fila_id " + "left join arena_setor on arena_setor.setor_id = arena_fila.setor_id " + "where acesso.status = 11 and acesso.caixa_id = " + CAIXA;
-            
+        string str = "select evento.titulo as Titulo, " +
+                "           ingresso.titulo as Ingresso, " + 
+                "           coalesce(ingressocategoria.preco,0) as Valor, " + 
+                "           acesso.acesso_id as Numero, " + 
+                "           campeonato.nrApoliceSeg as Apolice," + 
+                "           campeonato.nomeSeguradora as Seguradora, " + 
+                "           acesso.chaveAcesso as Barcode, " + 
+                "           arena_tipoingresso.titulo as legenda,   " + 
+                "           case when coalesce(ingressocategoria.preco, 0) = 0 then 'VENDA PROIBIDA' ELSE ' ' END AS Observacao, " + 
+                "           coalesce(arena_setor.nome,'') as Setor, " + 
+                "           coalesce(arena_fila.identificador,'') as Fila, " + 
+                "           coalesce(arena_cadeira.identificador,'') Cadeira, " + 
+                "           concat(evento.data, ' ', evento.horario) as DataHora, " + 
+                "           categoria.titulo, " + 
+                "           acesso.acesso_id " + 
+                "from acesso " + 
+                "left join evento                on evento.evento_id = acesso.evento_id " + 
+                "inner join arena_tipoingresso on arena_tipoingresso.tpingresso_id = acesso.tpingresso_id " + 
+                "inner join ingressocategoria on ingressocategoria.ingressocategoria_id = acesso.ingressocategoria_id" + 
+                " inner join ingresso on ingresso.ingresso_id = ingressocategoria.ingresso_id " + 
+                "inner join campeonato on campeonato.campeonato_id = evento.campeonato_id " + 
+                "inner join categoria on categoria.categoria_id = ingressocategoria.categoria_id " + 
+                "left join arena_cadeira on arena_cadeira.cadeira_id = acesso.cadeira_id " + 
+                "left join arena_fila on arena_fila.fila_id = arena_cadeira.fila_id " + 
+                "left join arena_setor on arena_setor.setor_id = arena_fila.setor_id " + 
+                "";
+
         List<frmPrincipal.ImpIngresso> impIngressoList = new List<frmPrincipal.ImpIngresso>();
             
         MySqlConnection conn = new MySqlConnection(stringConexao);
@@ -116,17 +121,18 @@ namespace VNJIngressos
 
     private void ImprimirIng(frmPrincipal.ImpIngresso ing)
         {
+            Console.Write(ing);
             /////////////////
             // MIOLO 
             /////////////////
             string canhoto = "<STX>L<CR>D11" +
-                "<CR>290000301900390" + ing.Titulo +
-                "<CR>290000201600370" + ing.Observacao +
-                "<CR>290000301600350" + ing.Observacao +
+                "<CR>290000301850390" + ing.Titulo +
+                "<CR>290000201300370" + ing.Setor +
+                "<CR>290000301500350" + ing.Observacao +
                 "<CR>290000301550335" + ing.DataHora +
                 "<CR>290000601500300" + "R$ " + ing.Valor +
                 "<CR>290000201600290" + ing.Seguradora +
-                "<CR>29000020140280" + ing.Apolice +
+                "<CR>290000201700278" + ing.Apolice +
                 "<CR>";
 
             /////////////////
@@ -150,7 +156,7 @@ namespace VNJIngressos
     {
       this.ImprimirIng(new frmPrincipal.ImpIngresso()
       {
-        Titulo = "LUIGI E MUITO ZIKA",
+        Titulo = "JUVENTUDE x PAYSANDU",
         Ingresso = "Arquibancada Descoberta",
         Valor = 100.00,
         Numero = 89257,
@@ -189,9 +195,7 @@ namespace VNJIngressos
       try
       {
                 //string CAIXA = this.comboBox1.SelectedValue.ToString();
-                String stringID  = comboBox1.Text;
-                string[] CAIXA = stringID.Split(" - ".ToCharArray());
-                this.BuscarImpressos(CAIXA[0]);
+                this.BuscarImpressos();
       }
       finally
       {
@@ -201,15 +205,9 @@ namespace VNJIngressos
 
     private void button2_Click(object sender, EventArgs e)
     {
-            if (this.comboBox1.SelectedIndex == 0)
-            {
-                MessageBox.Show("Não é possível iniciar a venda sem informar um caixa!");
-            } else
-            {
                 this.timer1.Enabled = true;
                 this.button2.Enabled = false;
-                this.button3.Enabled = true;
-            }
+            this.button3.Enabled = true;
     }
 
     private void button3_Click(object sender, EventArgs e)
@@ -233,8 +231,6 @@ namespace VNJIngressos
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -245,6 +241,7 @@ namespace VNJIngressos
             this.button1.TabIndex = 0;
             this.button1.Text = "Testar Impressão";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // timer1
@@ -273,37 +270,17 @@ namespace VNJIngressos
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(27, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(230, 21);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Selecione o seu caixa:";
-            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "frmPrincipal";
             this.Text = "Integrador de Impressões";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
